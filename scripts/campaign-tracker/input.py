47e23cfc44a1e4b621cada442a197033ec390bc2
Afif Nahas <afifnahas1998@gmail.com>
2024-07-26 16:54:00 +0300

    adding segments folders and arrangind some files

---
+++ scripts/campaign-tracker/input.py
@@ -0,0 +1,11 @@
+code_name = "" # 12345
+
+campaign_name = "" # MyNameIsJeff
+
+countries = [] # "UAE" ,"KSA"
+
+start_date = "" # "2024-06-10"
+
+end_date = "" # "2024-06-30"
+
+type = "" # Dashboard or Placelift
---
+++ scripts/segments/setup.py
@@ -0,0 +1,28 @@
+import os
+
+def create_directory_structure(base_path):
+    # Define the directory paths relative to the base path
+    directory_paths = [
+        os.path.join(base_path, 'data'),
+        os.path.join(base_path, 'data/raw'),
+        os.path.join(base_path, 'data/served'),
+        os.path.join(base_path, 'data/controlled')
+    ]
+
+    for folder_path in directory_paths:
+        # Check if the directory already exists
+        if not os.path.exists(folder_path):
+            # If it doesn't exist, create it
+            os.makedirs(folder_path)
+            print(f"Directory '{folder_path}' was created.")
+        else:
+            # If it exists, print a message
+            print(f"Directory '{folder_path}' already exists.")
+
+def main():
+    # Define the base path
+    base_path = os.path.join('scripts', 'segments')
+    create_directory_structure(base_path)
+
+if __name__ == "__main__":
+    main()
