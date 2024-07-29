96e6dd5b2c0c40b502832c0dc278b4e2c53151c7
AfifNahas <cloudnahas-m@afif-laptop.omtgroup.local>
2024-07-29 08:36:02 +0300

    Saving my last

---
+++ scripts/pre-campaign/input.py
@@ -0,0 +1,24 @@
+code_name = '114'
+
+backend_reports = '53915'
+
+campaign_name = "Oriental Weavers" # Testing Phase
+
+countries = ['EGY'] # UAE
+
+segments = []  
+#"Fashion Shoppers",
+
+time_interval = 0
+
+excluded_segments = []
+#,"McDonald's Loyal Clients"
+
+start_date = '2024-06-05'
+
+end_date = '2024-07-06'
+
+# Dashboard or Placelift
+type = 'Placelift'
+
+controlled_size = 10000
