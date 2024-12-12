04b5b117037fc96da5f96f8b582114e690509582
Afif Nahas <afifnahas1998@gmail.com>
2024-12-12 16:26:40 +0200

    check on friday

--- projects/campaign-tracker/input.py
+++ projects/campaign-tracker/input.py
@@ -1,15 +1,15 @@
-campaign_name = "Centrum EOC" # MyNameIsJeff
+campaign_name = "IHG" # MyNameIsJeff
 
-countries = ["KSA"] # "UAE" ,"KSA"
+countries = ["KSA","KSA","KSA"] # "UAE" ,"KSA"
 
-start_date = "2024-11-01" # "2024-06-10"
+start_date = "2024-07-08" # "2024-06-10"
 
 end_date = "2024-11-30" # "2024-06-30"
 
 type = "Placelift" # Dashboard or Placelift
 
-backend_reports = [58433]
+backend_reports = [54792, 54895, 54896]
 
 time_interval = -1
 
-has_segments = 0
+has_segments = 1
