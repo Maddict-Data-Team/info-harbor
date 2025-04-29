8e435f04b9646a73097dd8b6dbfbf81bdd180add
Afif Nahas <afifnahas1998@gmail.com>
2025-04-29 10:57:41 +0300

    Clean folder added

--- projects/segments/input.py
+++ projects/segments/input.py
@@ -1,39 +1,29 @@
-code_name = "132" # 122
+code_name = "144"  # 122
 
-campaign_name = "Infinity" # Redoxon
+campaign_name = "Marvel-Thunderbolts"  # Redoxon
 
-countries = ["KWT"]#,"","","",""] # ,
+countries = ["KSA", "UAE", "KWT"]  # ,"","","",""] # ,
 
-segments = ["custom_Business"]
-# "Gamers and Activities Frequenters","University Attendees","custom_Foodies","Families","Fast Food Lovers", 
-# KSA 
-# # z
+segments = [
+    "Young Cosmopolitans",
+    "Cinema Goers",
+    "Gamers and Activities Frequenters",
+    "Fast Food Lovers",
+]
+# "Gamers and Activities Frequenters","University Attendees","custom_Foodies","Families","Fast Food Lovers",
 
-# KSA 
 
 # "custom_KFC stores","custom_Fine Dining Night Club"
-dict_custom_segments = { 
-    "Custom_QSR Frequenters":{
-        "type":"POI",
-        "Category":["QSR"],
-        "radius":50
+dict_custom_segments = {
+    "Custom_QSR Frequenters": {"type": "POI", "Category": ["QSR"], "radius": 50},
+    "custom_Foodies": {"type": "POI", "General_Category": ["Food"], "radius": 50},
+    "custom_Competitor Car Owners": {
+        "type": "POI",
+        "General_Category": ["Automotive"],
+        "Chain": ["Nissan", "Ford", "Tesla", "Jeep"],
+        "radius": 160,
     },
-    "custom_Foodies":{
-        "type":"POI",
-        "General_Category":["Food"],
-        "radius":50
-    },
-        "custom_Competitor Car Owners":{
-        "type":"POI",
-        "General_Category":["Automotive"],
-        "Chain":["Nissan","Ford","Tesla","Jeep"],
-        "radius":160
-    },
-    "custom_Business":{
-        "type":"POI",
-        "General_Category":["Business"],
-        "radius":80
-    },
+    "custom_Business": {"type": "POI", "General_Category": ["Business"], "radius": 80},
     # "custom_Fine Dining Night Club":{
     #     "type":"POI",
     #     "General_Category":["food"],
@@ -43,7 +33,7 @@ dict_custom_segments = {
     # }
 }
 
-excluded_segments = [] #  "Male"
+excluded_segments = []  #  "Male"
 
 controlled_size = 10000
 
